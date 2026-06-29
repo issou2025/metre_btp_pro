39397c83906802b45516fc10d70e0b56a6a0694e
issou2025 <issou2025@example.com>
2026-06-30 00:30:15 +0100

    Configuration de la signature et generation du fichier keystore pour la build AAB

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,11 +30,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "upload"
+            keyPassword = "metre_btp_password"
+            storeFile = file("upload-keystore.jks")
+            storePassword = "metre_btp_password"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
